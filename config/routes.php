4dd951ece547841317df39e9c2aeef16150bcc66
LocmanMR <47283337+LocmanMR@users.noreply.github.com>
2019-04-10 06:56:11 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -7,17 +7,9 @@ return [
     'news/edit/delete/([0-9]+)' => 'news/delete/$1',
     'news/tag' => 'news/tag',
     'news/edit' => 'news/edit',
-
-    'admin/product/create' => 'adminProduct/create',
-    'admin/product/update/([0-9]+)' => 'adminProduct/update/$1',
-    'admin/product/delete/([0-9]+)' => 'adminProduct/delete/$1',
-    'admin/product' => 'adminProduct/index',
-
     // Новости:
     'news/([a-z]+)' => 'news/tags/$1',
 	'news' => 'news/index',
-
-
     // Пользователь
     'user/register' => 'user/register',
     'user/login' => 'user/login',
